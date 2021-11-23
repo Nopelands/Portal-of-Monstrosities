34f00e61f74aecb74375c4f0f483e3b8d4ff1020
Nopelands <rhas@cin.ufpe.br>
2021-11-22 21:59:10 -0300

    Add StatBlock attributes and constructor attribute lists

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 class StatBlock:
     name = str
-    def __init__(self):
-        self.name = 'todo' #TODO
+    armor_class = int
+    hit_points = int
+    attack_bonus = int
+    damage_output = int
+    proficiency_bonus = int
+    save_dc = int
+
+    def __init__(self, challenge_rating: int):
+        self.name = 'todo'  # TODO
+        ac_list = [10, 13, 13, 13, 14, 15, 15, 15, 16, 16, 17, 17, 17, 18, 18, 18, 18, 19]  # 17 or higher = 19
+        hp_list = [6, 85, 100, 115, 130, 145, 160, 175, 190, 205, 220, 235, 250, 265, 280, 295, 310, 325,
+                   340, 355, 400, 445, 490, 535, 580, 625, 670, 715, 760, 805, 850]
+        atk_list = [3, 3, 3, 4, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 8, 9, 10, 10, 10, 10, 11, 11, 11, 12, 12, 12, 13, 13,
+                    13, 14]
+        dmg_list = [1, 14, 20, 26, 32, 38, 44, 50, 56, 62, 68, 74, 80, 86, 92, 98, 104, 110, 116, 122, 140, 158, 176,
+                    194, 212, 230, 248, 266, 284, 302, 320]
+        prof_list = [2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9]
+        dc_list = [10, 13, 13, 13, 14, 15, 15, 15, 16, 16, 17, 17, 17, 18, 18, 18, 18, 19, 19, 19, 19, 20, 20, 20, 21,
+                   21, 21, 22, 22, 22, 23]
+
 
     def print(self):
         print(self.name)
 
+
 def statblock_gen(challenge_rating: int) -> StatBlock:
+    if challenge_rating > 30:
+        raise ValueError('CR can\'t be higher than 30')
     work_statblock = StatBlock()
-    #do things
+    # do things
     return work_statblock
 
 
 if __name__ == '__main__':
     CR_test = 10
-    Statblock_test = statblock_gen(CR_test)
+    try:
+        Statblock_test = statblock_gen(CR_test)
+    except ValueError:
+        #TODO
+        pass
 
